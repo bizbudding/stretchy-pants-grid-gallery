ed7cefba82b4f82ef2ed5a127d104e3e396d49b9
JiveDig <jivetheuniversal@gmail.com>
2025-02-28 15:46:35 -0500

    revert

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'e0a0b157a34886e04933');
+<?php return array('dependencies' => array(), 'version' => 'ae57843ab5db60b24a1f');
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-primitives'), 'version' => '6ad099254504864e3e7a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-primitives'), 'version' => 'f33a1afcaa43ba4d37dc');
