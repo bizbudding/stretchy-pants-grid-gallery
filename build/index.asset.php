19aa06aaeb816453a75229f92440ed5f24fa8f00
JiveDig <jivetheuniversal@gmail.com>
2025-03-31 17:02:49 -0400

    remove version from block.json so it uses plugin version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '3629a987de097693c30e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '5ce5deee7b6ec5579b7c');
